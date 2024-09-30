6fe55f528433642f607081f76c5c10c1d9df7028
Jose Alejandro Moreno <166447988+JochiKum@users.noreply.github.com>
2024-09-30 22:18:14 +0000

    fetch: star wars restApi en proceso

--- src/api/models.py
+++ src/api/models.py
@@ -1,22 +1,25 @@
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy import DateTime
 
 db = SQLAlchemy()
 
 
 class Users(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
     is_admin = db.Column(db.Boolean(), unique=False, nullable=False)
-    name = db.Column(db.String(), unique=False, nullable=True)
+    username = db.Column(db.String(), unique =False, nullable=True)
+    fist_name = db.Column(db.String(), unique=False, nullable=True)
+    last_name = db.Column(db.String(), unique=False, nullable=True)
     address =  db.Column(db.String(), unique=False, nullable=True)
     identification_type = db.Column(db.Enum('DNI', 'NIE', 'GC', name='identification_type'))
     identification_number = db.Column(db.Integer)
 
     def __repr__(self):
-        return f'<User {self.email} - {self.name}>'
+        return f'<User {self.email} - {self.firstname}>'
 
     def serialize(self):
         # Do not serialize the password, its a security breach
@@ -27,6 +30,7 @@ class Users(db.Model):
 
 
 class Followers(db.Model):
+    __tablename__ = 'followers'
     id = db.Column(db.Integer, primary_key=True)
     follower_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     follower_to = db.relationship('Users', foreign_keys=[follower_id], backref=db.backref('follower_to', lazy='select'))
@@ -65,3 +69,30 @@ class Books(db.Model):
     def serialize(self):
         return {'id': self.id,
                 'book': self.name}
+
+class Post(db.Model):
+    __tablename__ = 'post'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), nullable=False)
+    description = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.DateTime(), default= False, nullable=False)
+    image = db.Column(db.String(250), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_to = db.relationship('Users', foreign_keys=[user_id], backref=db.backref('post_to', lazy='select'))
+
+    def __repr__(self):
+        return f'<Post> {self.id} / {self.text_post}'
+
+    def serialize(self):
+        return {'id': self.id,
+                'post': self.text_post}
+
+class Comments(db.Model):
+    __tablename__ = 'comment'
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(250), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_to = db.relationship('Users', foreign_keys=[user_id], backref=db.backref('comment_to', lazy='select'))
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+    post_to = db.relationship('post', foreign_keys=[post_id], backref=db.backref('comments_to', lazy='select'))
