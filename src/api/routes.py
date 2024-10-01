cd66807038ec426a925be804d851081ef38d022a
Jose Alejandro Moreno <166447988+JochiKum@users.noreply.github.com>
2024-10-01 00:36:24 +0000

    fetch:Autentications

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,9 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import get_jwt_identity
 from api.models import db, Users, Authors
 
 
@@ -15,6 +18,48 @@ CORS(api)  # Allow CORS requests to this API
 def handle_hello():
     response_body = {}
     response_body['message'] = "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+    return response_body, 200
+
+
+@api.route("/login", methods=["POST"])
+def login():
+    response_body = {}
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    user = db.session.execute(db.select(Users).where(Users.email == email, Users.password == password, Users.is_active)).scalar()
+    print(user)
+    if not user:
+        response_body['message'] = 'bad email or password'
+        return response_body, 401
+    access_token = create_access_token(identity={'email': email, 'id': user.id, 'is_admin': user.is_admin})
+    response_body['message'] = 'usuario logeado con exito'
+    response_body['acces_token'] = access_token
+    return response_body, 200
+
+
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    response_body = {}
+    current_user = get_jwt_identity()
+    print(current_user)
+    response_body['message'] = 'Usuario con acceso'
+    response_body['logged_in_as'] = current_user
+    return response_body, 200
+
+
+@api.route("/profile", methods=["GET"])
+@jwt_required()
+def profile():
+    # Access the identity of the current user with get_jwt_identity
+    response_body = {}
+    current_user = get_jwt_identity()
+    print(current_user)
+    user = db.session.execute(db.select(Users).where(Users.id == current_user['id'])).scalar()
+    print(user.serialize())
+    response_body['message'] = 'Usuario con acceso'
+    response_body['logged_in_as'] = current_user
     return response_body, 200
 
 
@@ -47,7 +92,9 @@ def authors():
 
 
 @api.route('/authors/<int:id>', methods=['GET', 'PUT', 'DELETE'])
+@jwt_required()
 def author(id):
+    current_user = get_jwt_identity()
     response_body = {}
     row = db.session.execute(db.select(Authors).where(Authors.id == id)).scalar()
     if not row:
@@ -55,7 +102,7 @@ def author(id):
         response_body['results'] = {}
         return response_body, 404
     if request.method == 'GET':
-        response_body['message'] = f'Mensaje desde el GET de {id}'
+        response_body['message'] = f'Usted no puede modificar, ver o borrar los datos de {id}'
         response_body['results'] = row.serialize()
         return response_body, 200
     if request.method == 'PUT':
