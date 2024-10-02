993b60ae8e26129b8d3c30e0c96f6e85702f1999
Jose Alejandro Moreno <166447988+JochiKum@users.noreply.github.com>
2024-10-02 22:26:49 +0000

    Cambios

--- src/front/js/pages/Home.jsx
+++ src/front/js/pages/Home.jsx
@@ -8,20 +8,43 @@ export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+		<div className="container">
+			<div class="login-form">
+				<main>
+					<h3>Login into account</h3>
+					<div class="box-input">
+						<i class="fa-regular fa-envelope"></i>
+						<input type="text" placeholder="Email Address" />
+					</div>
+					<div class="box-input">
+						<i class="fa-solid fa-key"></i>
+						<input type="password" placeholder="Password" />
+					</div>
+					<label class="remember">
+						<input type="checkbox" />
+						Remember me
+					</label>
+					<button class="login-button">Login</button>
+					<a href="recover-password.html" class="forgot">Forget password?</a>
+					<p class="social-text">or login with</p>
+					<div class="social-login">
+						<a href="https://facebook.com" class="btn-facebook">
+							<i class="fa-brands fa-facebook"></i>
+							Facebook
+						</a>
+						<a href="https://google.com" class="btn-google">
+							<i class="fa-brands fa-google"></i>
+							Google
+						</a>
+						<a href="https://twitter.com" class="btn-twitter">
+							<i class="fa-brands fa-twitter"></i>
+							Twitter
+						</a>
+					</div>
+					<hr />
+					<p class="register-text">Don't have an account? <a href="register.html">Register here</a></p>
+				</main>
 			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
 		</div>
-	);
-};
+	)
+}
